bbe8bdd5637cd350a9b1a68fc96b6db8af973207
Sergey Petrov <desoulate777@gmail.com>
2017-12-01 23:16:41 +0300

    Add admin classes

--- datasets/admin.py
+++ datasets/admin.py
@@ -3,6 +3,22 @@ from django.contrib import admin
 from .models import Dataset, TestProcess, TestResult
 
 
+class TestResultInline(admin.StackedInline):
+
+    model = TestResult
+    readonly_fields = ['data', 'result', 'status', 'created', 'dataset']
+
+    def has_add_permission(self, request):
+        return False
+
+
+class TestProcessAdmin(admin.ModelAdmin):
+
+    inlines = [TestResultInline]
+
+    def has_add_permission(self, request):
+        return False
+
+
 admin.site.register(Dataset)
-admin.site.register(TestProcess)
-admin.site.register(TestResult)
+admin.site.register(TestProcess, TestProcessAdmin)
